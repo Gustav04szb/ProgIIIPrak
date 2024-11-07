a767d0916c72e3cf227957d9521aa7de99f31472
e342b150 0080702f <ms21-278457@htw-dresden.de>
2024-11-07 15:10:11 +0100

    Verbesserter Javagotchi

---
+++ src/JavaGotchi.java
@@ -0,0 +1,61 @@
+public class JavaGotchi {
+    enum Zustand { HUNGER, VERHUNGERT, ESSEN, GLÜCKLICH }
+    private static final int HUNGER_THRESHOLD = 15;
+    private static final int VERHUNGERT_THRESHOLD = 30;
+    private static final double ESSEN_CHANCE = 0.15;
+    private static final int HUNGER_INCREMENT_MAX = 5;
+    private static final int LOOP_DELAY_MS = 400;
+
+    private Zustand zustand = Zustand.GLÜCKLICH;
+    private int hunger = 0;
+    private boolean spielLaeuft = true;
+
+    private void aktuellerZustand(){
+        switch (zustand) {
+            case HUNGER:
+                System.out.println(":/ ... hunger");
+                if (Math.random()<ESSEN_CHANCE)
+                    zustand = Zustand.ESSEN;
+                break;
+            case VERHUNGERT:
+                System.out.println("x(");
+                spielLaeuft = false;
+                break;
+            case ESSEN:
+                System.out.println(":# ... essen");
+                hunger = 0;
+                zustand = Zustand.GLÜCKLICH;
+                break;
+            case GLÜCKLICH:
+                System.out.println(":)");
+        }
+    }
+    private void aktualisiereZustand() {
+        if (zustand == Zustand.HUNGER && Math.random() < ESSEN_CHANCE) {
+            zustand = Zustand.ESSEN;
+            hunger = 0; // Hunger zurücksetzen, wenn etwas Essbares gefunden wird
+        }
+    }
+    private void steigereHunger() {
+        if (zustand != Zustand.ESSEN) {
+            hunger += (int) (Math.random() * HUNGER_INCREMENT_MAX);
+            if (hunger > VERHUNGERT_THRESHOLD) {
+                zustand = Zustand.VERHUNGERT;
+            } else if (hunger > HUNGER_THRESHOLD) {
+                zustand = Zustand.HUNGER;
+            }
+        }
+    }
+    public void Einschalten(){
+        while(spielLaeuft){
+            aktuellerZustand();
+            aktualisiereZustand();
+            steigereHunger();
+            try {
+                Thread.sleep(LOOP_DELAY_MS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
