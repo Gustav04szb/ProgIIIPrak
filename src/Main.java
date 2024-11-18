4a2b84befeab22d082de9bc095d2ca30a51d24bc
e342b150 0080702f <ms21-278457@htw-dresden.de>
2024-11-18 18:10:10 +0100

    Cleared due to Project restructure

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        progPrakBeispiele bsp = new progPrakBeispiele();
-        /* 02 */
-//        bsp.boolAuswerten();
-
-        /* 03 */
-//        int aIntNumber = 5;
-//        float aFloatNumber = 2.8f;
-//
-//        System.out.println(bsp.formelf1x(aIntNumber));
-//        System.out.println(bsp.formelf1x(aFloatNumber));
-//        System.out.println(bsp.formelf2x(aIntNumber));
-//        System.out.println(bsp.formelf2x(aFloatNumber));
-//        System.out.println(bsp.formelf3x(aIntNumber));
-//        System.out.println(bsp.formelf3x(aFloatNumber));
-
-//        System.out.println( progPrakBeispiele().fakultaet(15));
-
-//        System.out.println(progPrakBeispiele.summenForLoopA());
-//        System.out.println(progPrakBeispiele.summenForLoopB());
-//        System.out.println(progPrakBeispiele.summenForLoopC());
-
-//        bsp.forInWhile1();
-//        bsp.forInWhile2();
-//        bsp.whileInFor();
-//
-//        bsp.LongToFloatCastin();
-        /* 05 */
-//        bsp.readFileFromArgs(args);
-//        bsp.writeToFileFromArgs(args);
-//        bsp.printArgsToConsol(args);
-
     }
 }
